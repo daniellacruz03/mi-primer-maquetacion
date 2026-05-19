a9333409e2e39777d728ee3c4325cca29a804b2b
Dani <daniellacruznieto@gmail.com>
2026-05-19 15:24:45 -0400

    update

--- sw.js
+++ sw.js
@@ -5,6 +5,11 @@ self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
+self.addEventListener('activate', (event) => {
+  // Toma el control de las páginas inmediatamente para activar la PWA
+  event.waitUntil(clients.claim());
+});
+
 self.addEventListener('fetch', (event) => {
   // Requisito mínimo para que Chrome permita la instalación:
   // Tener un manejador de fetch, aunque solo deje pasar la petición.
