8126834332bba19f2a1554deb2fef788fd784b87
Dani <daniellacruznieto@gmail.com>
2026-05-19 16:37:26 -0400

    app

--- sw.js
+++ sw.js
@@ -1,17 +1,13 @@
-const CACHE_NAME = 'bh-v1';
-
 self.addEventListener('install', (event) => {
-  // Instala el service worker inmediatamente
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  // Toma el control de las páginas inmediatamente para activar la PWA
   event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
-  // Requisito mínimo para que Chrome permita la instalación:
-  // Tener un manejador de fetch, aunque solo deje pasar la petición.
-  event.respondWith(fetch(event.request));
+    // El evento fetch es obligatorio para que el navegador 
+    // elimine el sello de 'acceso directo' y lo convierta en App.
+    event.respondWith(fetch(event.request));
 });
